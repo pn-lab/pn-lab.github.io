5a57563b84939b7b072f0b70b1dc0f0b31284ec1
yamaken <k.yamamoto0105@gmail.com>
2015-11-05 14:18:32 +0900

    no message

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(function() {
+	$('div.mdl-select > ul > li').click(function(e) {
+		var text = $(e.target).text();
+		$(e.target).parents('.mdl-select').addClass('is-dirty').children('input').val(text);
+	});
+	
+	$(document).ready(function() {
+        $("#lightgallery").lightGallery();
+    });
+});
